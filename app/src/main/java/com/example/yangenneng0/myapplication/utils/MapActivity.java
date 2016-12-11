31a03c10c65237979e4b7a347ac4f5c7ed5e1c09
yangenneng <1274491995@qq.com>
2016-12-11 13:43:19 +0800

    完善高德地图功能，实现准确定位，编辑定位信息

--- app/src/main/java/com/example/yangenneng0/myapplication/utils/MapActivity.java
+++ app/src/main/java/com/example/yangenneng0/myapplication/utils/MapActivity.java
@@ -201,7 +201,7 @@ public class MapActivity  extends AppCompatActivity implements LocationSource, A
         //标题
         options.title(buffer.toString());
         //子标题
-        options.snippet("这里好火");
+        options.snippet("我在这里");
         //设置多少帧刷新一次图片资源
         options.period(60);
 
