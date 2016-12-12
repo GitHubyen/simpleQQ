c504d4d30ec6bfbbeebe1da26c41f10a8bcac3cc
yangenneng <1274491995@qq.com>
2016-12-12 15:22:39 +0800

    悬浮按钮基本样式添加成功

--- app/src/main/java/com/example/yangenneng0/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yangenneng0/myapplication/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.yangenneng0.myapplication;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -37,16 +35,16 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //悬浮按钮
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //界面底部弹出信息 比toast更加好,毕竟snackbar 可以响应点击事件
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ////悬浮按钮
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        //界面底部弹出信息 比toast更加好,毕竟snackbar 可以响应点击事件
+        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //                .setAction("Action", null).show();
+        //    }
+        //});
 
         //Android官方侧滑菜单DrawerLayout
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
