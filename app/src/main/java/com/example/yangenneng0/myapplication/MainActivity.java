d5eab9c4479e07975360b3a15ac9193c6430fd53
yangenneng <1274491995@qq.com>
2016-12-13 11:35:09 +0800

    联系人列表新增--初始化

--- app/src/main/java/com/example/yangenneng0/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yangenneng0/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.yangenneng0.myapplication;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,19 +13,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AbsListView;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.example.yangenneng0.myapplication.adapter.PersonAdapter;
+import com.example.yangenneng0.myapplication.model.Person;
 import com.example.yangenneng0.myapplication.utils.CameraActivity;
 import com.example.yangenneng0.myapplication.utils.MapActivity;
 import com.example.yangenneng0.myapplication.utils.MessageActivity;
-import com.example.yangenneng0.myapplication.viewUI.ChatMainActivity;
+import com.example.yangenneng0.myapplication.utils.WordsNavigation;
 import com.example.yangenneng0.myapplication.viewUI.RegistActivity;
 import com.example.yangenneng0.myapplication.viewUI.SettingsActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * 功能：软件主界面类
  */
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {  //侧拉菜单滑出来的那一部分属于NavigationView
+        implements  WordsNavigation.onWordsChangeListener,
+        NavigationView.OnNavigationItemSelectedListener , AbsListView.OnScrollListener{
+    //侧拉菜单滑出来的那一部分属于NavigationView
+
+
+    //---联系人列表属性
+    private Handler handler;
+    private List<Person> list;
+    private TextView tv;
+    private ListView listView;
+    private WordsNavigation word;
+    //---联系人列表属性
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +55,21 @@ public class MainActivity extends AppCompatActivity
         //顶部工具栏
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //--------------联系人
+        tv = (TextView) findViewById(R.id.tv);
+        word = (WordsNavigation) findViewById(R.id.words);
+        listView = (ListView) findViewById(R.id.list);
+        //--------------联系人
+        //初始化数据
+        initData();
+        //初始化列表
+        initListView();
+        //设置列表点击滑动监听
+        handler = new Handler();
+        word.setOnWordsChangeListener(this);
+        //--------------联系人
+
 
         ////悬浮按钮
         //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -58,16 +94,16 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        //聊天
-        Button button= (Button) findViewById(R.id.goChat);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, ChatMainActivity.class);
-                MainActivity.this.startActivity(intent);
-            }
-        });
+        ////聊天
+        //Button button= (Button) findViewById(R.id.goChat);
+        //button.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent intent = new Intent();
+        //        intent.setClass(MainActivity.this, ChatMainActivity.class);
+        //        MainActivity.this.startActivity(intent);
+        //    }
+        //});
 
     }
 
@@ -190,5 +226,114 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+
+
+    //继承 WordsNavigation.onWordsChangeListener
+    //手指按下字母改变监听回调
+    @Override
+    public void wordsChange(String words) {
+        updateWord(words);
+        updateListView(words);
+    }
+
+    private void initListView() {
+        PersonAdapter adapter = new PersonAdapter(this, list);
+        listView.setAdapter(adapter);
+        listView.setOnScrollListener(this);
+    }
+
+    /**
+     * 初始化联系人列表信息
+     */
+    private void initData() {
+        list = new ArrayList<>();
+        list.add(new Person("Dave"));
+        list.add(new Person("张晓飞"));
+        list.add(new Person("杨光福"));
+        list.add(new Person("阿钟"));
+        list.add(new Person("胡继群"));
+        list.add(new Person("徐歌阳"));
+        list.add(new Person("钟泽兴"));
+        list.add(new Person("宋某人"));
+        list.add(new Person("刘某人"));
+        list.add(new Person("Tony"));
+        list.add(new Person("老刘"));
+        list.add(new Person("隔壁老王"));
+        list.add(new Person("安传鑫"));
+        list.add(new Person("温松"));
+        list.add(new Person("成龙"));
+        list.add(new Person("那英"));
+        list.add(new Person("刘甫"));
+        list.add(new Person("沙宝亮"));
+        list.add(new Person("张猛"));
+        list.add(new Person("张大爷"));
+        list.add(new Person("张哥"));
+        list.add(new Person("张娃子"));
+        list.add(new Person("樟脑丸"));
+        list.add(new Person("吴亮"));
+        list.add(new Person("Tom"));
+        list.add(new Person("阿三"));
+        list.add(new Person("肖奈"));
+        list.add(new Person("贝微微"));
+        list.add(new Person("赵二喜"));
+        list.add(new Person("曹光"));
+        list.add(new Person("姜宇航"));
+
+        //对集合排序
+        Collections.sort(list, new Comparator<Person>() {
+            @Override
+            public int compare(Person lhs, Person rhs) {
+                //根据拼音进行排序
+                return lhs.getPinyin().compareTo(rhs.getPinyin());
+            }
+        });
+    }
+
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        //当滑动列表的时候，更新右侧字母列表的选中状态
+        word.setTouchIndex(list.get(firstVisibleItem).getHeaderWord());
+    }
+
+    /**
+     * @param words 首字母
+     */
+    private void updateListView(String words) {
+        for (int i = 0; i < list.size(); i++) {
+            String headerWord = list.get(i).getHeaderWord();
+            //将手指按下的字母与列表中相同字母开头的项找出来
+            if (words.equals(headerWord)) {
+                //将列表选中哪一个
+                listView.setSelection(i);
+                //找到开头的一个即可
+                return;
+            }
+        }
+    }
+
+    /**
+     * 更新中央的字母提示
+     *
+     * @param words 首字母
+     */
+    private void updateWord(String words) {
+        tv.setText(words);
+        tv.setVisibility(View.VISIBLE);
+        //清空之前的所有消息
+        handler.removeCallbacksAndMessages(null);
+        //1s后让tv隐藏
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                tv.setVisibility(View.GONE);
+            }
+        }, 500);
+    }
 
 }
---
+++ app/src/main/java/com/example/yangenneng0/myapplication/adapter/PersonAdapter.java
@@ -0,0 +1,87 @@
+package com.example.yangenneng0.myapplication.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import com.example.yangenneng0.myapplication.R;
+import com.example.yangenneng0.myapplication.model.Person;
+
+import java.util.List;
+
+/**
+ * User: yangenneng
+ * DateTime: 2016/12/13 10:41
+ * Description:联系人ListView列表适配器
+ */
+public class PersonAdapter  extends BaseAdapter {
+
+    private List<Person> list;
+    private LayoutInflater inflater;
+
+    public PersonAdapter(Context context, List<Person> list) {
+        inflater = LayoutInflater.from(context);
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder; //自定义内部内类
+        if(null==convertView){
+            holder= new ViewHolder();
+            convertView=inflater.inflate(R.layout.person_list_item,null);//单个联系人
+            holder.tv_name= (TextView) convertView.findViewById(R.id.tv_name);//姓名
+            holder.tv_word= (TextView) convertView.findViewById(R.id.tv_word);//名字字母
+            convertView.setTag(holder);
+        }else {
+            holder= (ViewHolder) convertView.getTag();
+        }
+
+        String word=list.get(position).getHeaderWord();//第一个字母
+        holder.tv_word.setText(word);
+        holder.tv_name.setText(list.get(position).getName());
+
+        //将相同字母开头的合并在一起
+        if(position==0){
+            //第一个是一定显示的
+            holder.tv_word.setVisibility(View.VISIBLE);
+        }else {
+            //后一个与前一个对比,判断首字母是否相同，相同则隐藏
+            String headword=list.get(position-1).getHeaderWord();
+            if(word.equals(headword)){
+                holder.tv_word.setVisibility(View.GONE);
+            }else {
+                holder.tv_word.setVisibility(View.VISIBLE);
+            }
+        }
+
+
+        return convertView;
+    }
+
+
+    private class ViewHolder {
+        private TextView tv_word;
+        private TextView tv_name;
+    }
+
+
+}
